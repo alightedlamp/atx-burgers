182bfe96edaab6a17b8d9f8f7b37bd79aeda148d
Philip Harrell <alightedlamp@gmail.com>
2018-02-17 18:01:19 -0600

    Update connection file

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers (
+CREATE TABLE burger (
     id INT AUTO_INCREMENT,
     name VARCHAR(55) NOT NULL,
     restaurant_id INT NOT NULL,
