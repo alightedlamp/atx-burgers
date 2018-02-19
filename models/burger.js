e0f98a4a833174e21fe724110f83fde398f85127
Philip Harrell <alightedlamp@gmail.com>
2018-02-19 08:44:10 -0600

    Remove unnecessary promises from model

--- models/burger.js
+++ models/burger.js
@@ -2,58 +2,48 @@ var orm = require('../config/orm.js');
 
 var burger = {
   all: function(tableName) {
-    return new Promise((resolve, reject) => {
-      orm.selectAll(tableName).then((data, err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
+    return orm.selectAll(tableName).then((data, err) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        return data;
+      }
     });
   },
   join: function(tableOneName, tableTwoName) {
-    return new Promise((resolve, reject) => {
-      orm.join(tableOneName, tableTwoName).then((data, err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
+    return orm.join(tableOneName, tableTwoName).then((data, err) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        return data;
+      }
     });
   },
   create: function(tableName, values) {
-    return new Promise((resolve, reject) => {
-      orm.insertOne(tableName, values).then((data, err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
+    return orm.insertOne(tableName, values).then((data, err) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        return data;
+      }
     });
   },
   update: function(tableName, values, id) {
-    return new Promise((resolve, reject) => {
-      orm.updateOne(tableName, values, id).then((data, err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
+    return orm.updateOne(tableName, values, id).then((data, err) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        return data;
+      }
     });
   },
   delete: function(tableName, id) {
-    return new Promise((resolve, reject) => {
-      orm.deleteOne(tableName, id).then((data, err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
+    return orm.deleteOne(tableName, id).then((data, err) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        return data;
+      }
     });
   }
 };
